b2ed43eb5b0f191b353ea0581b63dd02a65a6c35
Nikolai Neawedde <nikoneawedde@gmail.com>
2026-09-13 12:05:32 -0500

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://vglmmpjegqfzjsoqqqso.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_R7Bp55mfzNe43L6_6rKbuw_7EUlKR8z";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
